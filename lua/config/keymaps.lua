a78ba8010da7d1c3b0115ca93e83aab341f9af28
GeoffreyDick <git@geoffdick.com>
2024-04-03 15:19:34 -0600

    feat(keymaps): add custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- map leader+w to save current file in normal mode
+vim.keymap.set("n", "<Leader>w", ":write<CR>", { noremap = true, silent = true, desc = "Save file" })
+
+-- map leader+y to copy to system clipboard in normal and visual mode
+vim.keymap.set({ "n", "v" }, "<Leader>y", '"+y', { noremap = true, silent = true, desc = "Yank to clipboard" })
+
+-- keep cursor in middle of screen when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
