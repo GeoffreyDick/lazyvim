404e6a1a7f0cfce409163b8b2cce74df1a347e30
GeoffreyDick <git@geoffdick.com>
2024-04-03 15:20:01 -0600

    feat: add custom vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- enable color column
+vim.opt.colorcolumn = "80"
+
+-- tab settings
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- set indenting behaviour
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- disable wrapping
+vim.opt.wrap = false
+
+-- offset scroll
+vim.opt.scrolloff = 8
