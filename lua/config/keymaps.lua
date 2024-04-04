9c65bd89aff871a9a2c4d9839f14f1d2d3181445
GeoffreyDick <git@geoffdick.com>
2024-04-04 07:30:02 -0600

    chore: format files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,10 +3,20 @@
 -- Add any additional keymaps here
 
 -- map leader+w to save current file in normal mode
-vim.keymap.set("n", "<Leader>w", ":write<CR>", { noremap = true, silent = true, desc = "Save file" })
+vim.keymap.set(
+	"n",
+	"<Leader>w",
+	":write<CR>",
+	{ noremap = true, silent = true, desc = "Save file" }
+)
 
 -- map leader+y to copy to system clipboard in normal and visual mode
-vim.keymap.set({ "n", "v" }, "<Leader>y", '"+y', { noremap = true, silent = true, desc = "Yank to clipboard" })
+vim.keymap.set(
+	{ "n", "v" },
+	"<Leader>y",
+	'"+y',
+	{ noremap = true, silent = true, desc = "Yank to clipboard" }
+)
 
 -- keep cursor in middle of screen when scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
