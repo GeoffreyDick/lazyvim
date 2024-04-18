57ead0ad5d32113e6e3ffc859861f16cf9708190
GeoffreyDick <git@geoffdick.com>
2024-04-18 16:41:44 -0600

    feat: add gruvbox

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = true,
+		opts = {
+			contrast = "hard",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
+}
