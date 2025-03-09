19964424d0ea1b9af8229c4bba2ecb31f3927b0d
GeoffreyDick <git@geoffdick.com>
2025-03-09 10:53:29 -0600

    feat: switch theme to gruvbox-material

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,18 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
+		"sainnhe/gruvbox-material",
+		lazy = false,
 		priority = 1000,
-		config = true,
-		opts = {
-			contrast = "hard",
-		},
+		config = function()
+			vim.g.gruvbox_material_enable_italic = true
+			vim.g.grubbox_material_background = "hard"
+			vim.g.gruvbox_material_transparent_background = 2
+		end,
 	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "gruvbox",
+			colorscheme = "gruvbox-material",
 		},
 	},
 }
